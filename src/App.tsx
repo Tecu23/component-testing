36c32fc2b37a9da30f21778025c8d131124baed5
Tecu23 <andrei.teculescu@gmail.com>
2023-04-11 22:39:58 +0300

    ScaleLoader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SubmitButton from "@/components/SubmitButton";
 import SyncLoader from "@/components/spinners/SyncLoaded";
+import ScaleLoader from "@/components/spinners/ScaleLoader";
 
 function App() {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <SyncLoader />
+            {/* <SyncLoader /> */}
+            <ScaleLoader />
         </div>
     );
 }
